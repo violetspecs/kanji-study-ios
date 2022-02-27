772e67bb54a27e3af034dc00c538b970b9a436cf
violetspecs <joseipong@gmail.com>
2022-02-27 19:54:28 +0800

    Initial Commit

---
+++ KanjiStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KanjiStudy
+//
+//  Created by Martin on 2/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
